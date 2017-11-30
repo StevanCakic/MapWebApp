109c665812446c8f34cda9544ccd03eecb3608e4
StevanCakic <stevancakic93@gmail.com>
2017-11-30 21:35:05 +0100

    initial commit

---
+++ src/components/app.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import WebMap from './map/WebMap';
+
+export default class App extends Component {
+  render() {
+    return (
+      <WebMap />
+    );
+  }
+}
---
+++ src/components/map/WebMap.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import ReactMapboxGl, { ZoomControl, Layer, Feature } from "react-mapbox-gl";
+
+const Map = ReactMapboxGl({
+  accessToken:
+    "pk.eyJ1IjoiaXJpc2QiLCJhIjoiY2o4OXp1cTFyMDhhNjJxcGl2Y2ticDdhayJ9.p6Gc-F2ylzEMfBN0XJSfeQ",
+  minZoom: 2,
+  maxZoom: 7,
+  zoom: [4]
+});
+
+class WebMap extends Component {
+  render() {
+    return (
+      <Map
+        style="mapbox://styles/irisd/cj8mzhg4u7c5k2rojd7r8iuw3"
+        containerStyle={{
+          height: "100vh",
+          width: "100%"
+        }}
+        center={[19.2593642, 42.4304196]}
+      >
+        <ZoomControl />
+      </Map>
+    );
+  }
+}
+
+export default WebMap;
